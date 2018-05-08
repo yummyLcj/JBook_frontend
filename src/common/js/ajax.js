bb05aaf79b5f1b6238c8bdc48047a6c7ad2256dd
yummyLcj <luchenjiemail@gmail.com>
2018-05-08 09:10:16 +0800

    bug fixed

--- src/common/js/ajax.js
+++ src/common/js/ajax.js
@@ -2,8 +2,8 @@ import wepy from 'wepy'
 
 export default (options = {}) => {
   return new Promise((resolve, reject) => {
-    // options.url = `http://localhost:3000${options.url}`
-    options.url = `http://ss.ylittle.com:3000${options.url}`
+    options.url = `http://localhost:3000${options.url}`
+    // options.url = `http://ss.ylittle.com:3000${options.url}`
     options.success = resolve
     options.fail = reject
     const data = options.data || {}
--- src/common/js/login.js
+++ src/common/js/login.js
@@ -1,26 +1,48 @@
 import wepy from 'wepy'
 import request from 'src/common/js/ajax.js'
 
-export default (cb) => {
-  wepy.login({
-    success (res) {
-      request({
-        url: '/session',
-        method: 'post',
-        data: {
-          code: res.code,
-          name: 'testName'
-        }
-      })
-        .then((res) => {
-          wepy.setStorage({
-            key: 'sessionUid',
-            data: res.data.uid,
-            success() {
-              typeof cb === 'function' && cb(res.data.uid)
-            }
-          })
-        })
+const getUserInfo = (cb) => (
+  new Promise((resolve) => {
+    wepy.getUserInfo({
+      success: resolve
+    })
+  })
+)
+
+const weLogin = (data, cb) => (
+  new Promise((resolve) => {
+    wepy.login({
+      success: resolve
+    })
+  })
+)
+
+const login = function (userInfo, res) {
+  request({
+    url: '/session',
+    method: 'post',
+    data: {
+      code: res.code,
+      name: userInfo.nickName
     }
   })
+    .then((res) => {
+      wepy.setStorage({
+        key: 'sessionUid',
+        data: res.data.uid,
+        success() {
+          typeof cb === 'function' && cb(res.data.uid)
+        }
+      })
+    })
+}
+
+export default (cb) => {
+  getUserInfo()
+    .then((res) => {
+      weLogin()
+        .then((res2) => {
+          login(res.userInfo, res2)
+        })
+    })
 }
